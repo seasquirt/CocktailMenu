862a39db1d8a40e6f68b47f25898af591ff0e958
YuhNeko <37379762+seasquirt@users.noreply.github.com>
2024-09-23 04:07:24 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
       tag.addEventListener('click', () => {
         tag.classList.toggle('selected');
         filterCocktails();
+        window.navigator.vibrate([20])
       });
     });
   
